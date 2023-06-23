42eb9da0c1e8249e62c786db329fd15cbcc75947
nashare <tatiana.lavr@gmail.com>
2023-06-22 17:56:09 -0700

    problem 2180

--- solutions.js
+++ solutions.js
@@ -19,20 +19,39 @@ var addDigits = function (num) {
 var getLucky = function (s, k) {
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let s_to_alph = ""
+    let sToAlph = ""
     for (let i = 0; i < s.length; i++) {
-        s_to_alph += alphabet.indexOf(s[i]) + 1;
+        sToAlph += alphabet.indexOf(s[i]) + 1;
     }
-    let result = s_to_alph;
+    let result = sToAlph;
     let c = 1;
     while (c <= k) {
         let subresult = 0
-        const str_result = result.toString();
-        for (let i = 0; i < str_result.length; i++) {
-            subresult += parseInt(str_result[i])
+        const strResult = result.toString();
+        for (let i = 0; i < strResult.length; i++) {
+            subresult += parseInt(strResult[i])
         }
         result = subresult;
         c += 1;
     }
     return result
 };
+
+// 2180. Count Integers With Even Digit Sum
+
+var countEven = function (num) {
+    let counter = 0;
+    for (let i = 1; i <= num; i++) {
+        let digitsSum = 0;
+        intToStr = i.toString()
+        for (let c = 0; c < intToStr.length; c++) {
+            digitsSum += parseInt(intToStr[c]);
+        }
+        if (digitsSum % 2 === 0) {
+            counter += 1
+        }
+    }
+    return counter;
+};
+
+console.log(countEven(4))
