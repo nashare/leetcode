c8e550792bb712eef1a59d1c388f9257c5ee756f
Tetiana Zagoruiko <36436308+nashare@users.noreply.github.com>
2023-08-29 12:26:03 -0700

    Time: 66 ms (83.78%) | Memory: 41.9 MB (84.68%) - LeetSync

---
+++ 2122-count-special-quadruplets/count-special-quadruplets.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countQuadruplets = function(nums) {
+    let result = 0;
+    for (let a = 0; a < nums.length - 3; a++) {
+        for (let b = a + 1; b < nums.length - 2; b++) {
+            for (let c = b + 1; c < nums.length - 1; c++) {
+                for (let d = c + 1; d < nums.length; d++) {
+                    if (nums[a] + nums[b] + nums[c] == nums[d]) {
+                        result++;
+                    }
+                }
+            }
+        }
+    }
+    return result;
+};
