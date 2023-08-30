3e23b7f5696c867bfb2bee420b20e7da7c3029b9
Tetiana Zagoruiko <36436308+nashare@users.noreply.github.com>
2023-08-30 00:24:48 -0700

    Time: 69 ms (44.67%) | Memory: 45.1 MB (89.34%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (num[i] % 2 != 0) {
+            let subNum = num.slice(0, i+1);
+            return subNum
+        }
+    }
+    return ""
+};
