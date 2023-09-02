e876c846d8421a59f8a5a1420d7496f3334d7cb0
Tetiana Zagoruiko <36436308+nashare@users.noreply.github.com>
2023-09-02 00:16:47 -0700

    Time: 95 ms (62.68%) | Memory: 46.5 MB (60.40%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -5,11 +5,9 @@
 var maximumProduct = function(nums) {
     nums.sort((a, b) => a - b);
     let numsLen = nums.length;
-    if (nums[0] < 0 && nums[1] < 0) {
-        if (nums[numsLen - 1] > 0) {
-            if ((nums[0] * nums[1]) > (nums[numsLen - 2] * nums[numsLen - 3])) {
-                return nums[numsLen - 1] * nums[0] * nums[1];
-            }
+    if (nums[0] < 0 && nums[1] < 0 && nums[numsLen - 1] > 0) {
+        if ((nums[0] * nums[1]) > (nums[numsLen - 2] * nums[numsLen - 3])) {
+            return nums[numsLen - 1] * nums[0] * nums[1];
         }
 
     }
